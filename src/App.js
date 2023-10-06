2e025ab9ff0e3b105c0231b012b49a375d59dff4
aakash <aakashramesh018@gmail.com>
2023-10-07 01:59:04 +0530

    added about page and dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-      <Navbar title = "Text Utils" aboutText = "About"/>
+      <Navbar title='Text Utils' aboutText="About" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to be analysed below :"/>
+        <TextForm heading="Enter the text to be analysed here :"/>
+        {/*<About/>*/}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,24 +3,45 @@ import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
   const handleOnChange = (event) => {
-    console.log("OnChange");
+    console.log("OnChange button was pressed");
     setText(event.target.value);
   }
-  const handleClick = () => {
-    console.log("Uppercase was pressed");
+  const handleUpClick = () => {
+    console.log("Uppercase button was pressed");
     let newText = text.toUpperCase();
     setText(newText);
   }
-  const[text , setText] = useState("Enter text here");
+  const handleLowClick = () => {
+    console.log("Lowercase button was pressed");
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+  const handleClearClick = () => {
+    console.log("Clear Text button was pressed");
+    setText("");
+  }
+  const[text , setText] = useState("");
   return (
     <>
-    <h1>{props.heading}</h1>
-    <div className="mb-3">
-      <label htmlFor="myBox" className="form-label"></label>
-      <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="10"></textarea>
-    </div>
-    <button className="btn btn-primary" onClick = {handleClick}>Convert To Upper Case</button>
-  </>
+      <div className="container">
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <label htmlFor="myBox" className="form-label"></label>
+        <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="10"></textarea>
+      </div>
+      <button className="btn btn-primary mx-2" onClick = {handleUpClick}>Convert To Upper Case</button>
+      <button className="btn btn-primary mx-2" onClick = {handleLowClick}>Convert To Lower Case</button>
+      <button className="btn btn-primary mx-2" onClick = {handleClearClick}>Clear Text</button>
+      </div>
+
+      <div className="container my-5">
+        <h1>Text Summary :</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>Approx. time taken to read the text : {(text.split(" ").length * 0.008)} minutes</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   )
 }
 
